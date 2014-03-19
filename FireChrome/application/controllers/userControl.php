7cd5c4abe18c4b5ee000b59c238ea5c7f6318526
marek pagel <pagel.marek@gmail.com>
2014-03-19 17:32:34 +0200

    usercontrol nupp näha vaid adminnidele, korrastasin sessiondata

--- FireChrome/application/controllers/userControl.php
+++ FireChrome/application/controllers/userControl.php
@@ -1,17 +1,35 @@
-<?php
+<?php if (! defined ( 'BASEPATH' )) exit ( 'No direct script access allowed' );
 
-if (! defined ( 'BASEPATH' ))
-	exit ( 'No direct script access allowed' );
+
 class userControl extends CI_Controller {
+	
+	private $sessionData = array();
+	
 	public function index() {
+		$this->sessionData = array(
+				'user_id' => $this->session->userdata('user_id'),
+				'username' => $this->session->userdata('username'),
+				'email' => $this->session->userdata('email'),
+				'level' => $this->session->userdata('level'),
+				'isLoggedIn' => $this->session->userdata('is_logged_in'),
+		);
+		
 		$this->load->model ( "UsersModel" );
 		$data ["users"] = $this->UsersModel->getAllUsers ();
 		$data ["confirmation"] = "";
-		$this->load->view ( "header", $data );
+		$this->load->view('header', $this->sessionData);
 		$this->load->view ( "UserControl", $data );
-		$this->load->view ( "footer", $data );
+		$this->load->view ( "footer");
 	}
 	public function updateUsers() {
+		$this->sessionData = array(
+				'user_id' => $this->session->userdata('user_id'),
+				'username' => $this->session->userdata('username'),
+				'email' => $this->session->userdata('email'),
+				'level' => $this->session->userdata('level'),
+				'isLoggedIn' => $this->session->userdata('is_logged_in'),
+		);
+
 		$this->load->model ( "UsersModel" );
 		
 		$newArray = array ();
@@ -23,14 +41,16 @@ class userControl extends CI_Controller {
 			);
 		}
 		$this->UsersModel->updateLevels ( $newArray );
+
+		
 		
 		$this->load->model ( "UsersModel" );
 		
 		$data ["users"] = $this->UsersModel->getAllUsers ();
 		$data ["confirmation"] = "Andmed muudetud";
 		;
-		$this->load->view ( "header", $data );
+		$this->load->view('header', $this->sessionData);
 		$this->load->view ( "UserControl", $data );
-		$this->load->view ( "footer", $data );
+		$this->load->view ( "footer");
 	}
 }
--- FireChrome/application/views/settings.php
+++ FireChrome/application/views/settings.php
@@ -1,27 +1,34 @@
 <h1>Seaded</h1>
-<a id="change-password-link" href="javascript:void(0);">Muuda parooli</a>
+<ul>
+	<li><a id="change-password-link" href="javascript:void(0);">Muuda
+			parooli</a></li>
 
 <?php
-	echo validation_errors();
+echo validation_errors ();
 
-	echo form_open('main/changePasswordValidation', array('id' => 'change-password-form'));
+echo form_open ( 'main/changePasswordValidation', array (
+		'id' => 'change-password-form' 
+) );
 
-	echo "<p>Praegune parool: ";
-	echo form_password('password');
-	echo "</p>";
+echo "<p>Praegune parool: ";
+echo form_password ( 'password' );
+echo "</p>";
 
-	echo "<p>Uus parool: ";
-	echo form_password('npassword');
-	echo "</p>";
+echo "<p>Uus parool: ";
+echo form_password ( 'npassword' );
+echo "</p>";
 
-	echo "<p>Uus parool uuesti: ";
-	echo form_password('anpassword');
-	echo "</p>";
+echo "<p>Uus parool uuesti: ";
+echo form_password ( 'anpassword' );
+echo "</p>";
 
-	echo "<p>";
-	echo form_submit('change_password_submit', 'Muuda parool');
-	echo "</p>";
+echo "<p>";
+echo form_submit ( 'change_password_submit', 'Muuda parool' );
+echo "</p>";
 
-	echo form_close();
-?>
-<a id="usercontroll" href="<?php echo base_url() . "UserControl" ?>">Halda Kasutajaid</a>
+echo form_close ();
+if ($level > 1) { ?>
+<li><a id="usercontrol" href="<?php echo base_url() . "UserControl" ?>">Halda
+			Kasutajaid</a></li>
+<?php } ?>
+</ul>
