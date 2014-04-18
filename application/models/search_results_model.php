143f962cdd27825f8cbf8fdc565e003819b29fad
Eerik Muuli <eerik.muuli@gmail.com>
2014-04-18 18:47:49 +0300

    New searching doesn't care about capitalization.

--- application/models/search_results_model.php
+++ application/models/search_results_model.php
@@ -12,7 +12,7 @@ class Search_results_model extends CI_Model {
         $search_result = array();
         $query = $this->db->query('SELECT title, id FROM news');
         foreach ($query->result_array() as $row) {
-            if (strpos($row['title'], $string) !== false) { //And is visible ka siia panna?   
+            if (strpos(strtolower($row['title']), strtolower($string)) !== false) { //And is visible ka siia panna?   
                 array_push($added, $row['id']);//Et ei tekiks kordusi!    
                 array_push($search_result, $row['id']);
             }
@@ -20,7 +20,7 @@ class Search_results_model extends CI_Model {
         $bool = false;
         $query = $this->db->query('SELECT content, id, title FROM news');
         foreach ($query->result_array() as $row) {
-            if (strpos($row['content'], $string) !== false) { //And is visible ka siia panna? 
+            if (strpos(strtolower($row['content']), strtolower($string)) !== false) { //And is visible ka siia panna? 
                 foreach($added as $rows) {
                     if ($row['id'] == $rows) {
                         $bool = true;
